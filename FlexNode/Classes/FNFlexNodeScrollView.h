38be8e5483c8f100173989ebcc3b66fb7906e48b
yinhao <yinhao@5eplay.com>
2020-04-10 20:07:52 +0800

    view

--- FlexNode/Classes/FNFlexNodeScrollView.h
+++ FlexNode/Classes/FNFlexNodeScrollView.h
@@ -11,6 +11,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface FNFlexNodeScrollView : UIScrollView
 @property(nonatomic, strong) FNFlexNode *node;
+@property(nonatomic, assign) UILayoutConstraintAxis direction;
 @end
 
 NS_ASSUME_NONNULL_END
--- FlexNode/Classes/FNFlexNodeScrollView.m
+++ FlexNode/Classes/FNFlexNodeScrollView.m
@@ -14,13 +14,22 @@
     if (self) {
         self.node = [[FNFlexNode alloc] initWithView:UIView.new];
         [self addSubview:self.node.view];
+        self.direction = -1;
     }
     return self;
 }
 - (void)layoutSubviews{
     [super layoutSubviews];
+    if(self.direction == UILayoutConstraintAxisVertical){
+        self.node.width = self.frame.size.width;
+    }else if (self.direction == UILayoutConstraintAxisHorizontal){
+        self.node.height = self.frame.size.height;
+    }else{
+        
+    }
     [self.node layout];
     self.contentSize = CGSizeMake(self.node.width, self.node.height);
+    
 }
 
 @end
